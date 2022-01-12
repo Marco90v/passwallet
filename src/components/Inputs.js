a9947d89d902795a14016db089bf122f819e7393
Marco Velasquez Figarella <marcovelasquez90@hotmail.com>
2022-01-12 14:26:45 -0400

    se modifico tipo de input a password para ocultar contraseña y se agrego boton para poder visualizar

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -1,7 +1,13 @@
 import { Col, Form, InputGroup, Row } from "react-bootstrap";
 import { FaClipboard } from "react-icons/fa";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+import { useState } from "react";
 
 const Inputs = ({input,setInput,disa,n_e,copiar}) => {
+
+    const [hideShow,sethideShow] = useState(false);
+
+    
     const recorrer = () =>{
         const arr = [];
         for (const k in input) arr.push(k);
@@ -24,6 +30,8 @@ const Inputs = ({input,setInput,disa,n_e,copiar}) => {
                 return e;
         }
     }
+
+    const changeState = (e) => e==="Password" ? (hideShow ? "text" : "password") : "text";
     
     return(
         <>{
@@ -34,7 +42,8 @@ const Inputs = ({input,setInput,disa,n_e,copiar}) => {
                         <Form.Label md={{ span: 2, offset: 3 }} column="sm" sm={2}><strong>{labelInputs(e)}</strong></Form.Label>
                         <Col sm={4}>
                             <InputGroup size="sm" >
-                                <Form.Control size="sm" type="text" placeholder={labelInputs(e)} name={e} value={input[e]} onChange={(ele)=>{setInput(ele)}} disabled={disa} />
+                                {(n_e && e==="Password") && <InputGroup.Text id="basic-addon2" className="hideShow" style={{backgroundColor: "white"}} onClick={()=>sethideShow(t=>!t)}> {hideShow ? <AiFillEyeInvisible/> : <AiFillEye />} </InputGroup.Text>}
+                                <Form.Control size="sm" type={changeState(e)} placeholder={labelInputs(e)} name={e} value={input[e]} onChange={(ele)=>{setInput(ele)}} disabled={disa} />
                                 {n_e && <InputGroup.Text id="basic-addon2" className="clipboard" style={{backgroundColor: "purple"}} onClick={()=>copiar(input[e])}> <FaClipboard /> </InputGroup.Text>}
                             </InputGroup>
                         </Col>
