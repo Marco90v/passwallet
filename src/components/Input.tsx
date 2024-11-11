d56b809fd99b59162dcd5a5bcec9ad0a9414adb8
Marco Velasquez Figarella <marcovelasquez90@hotmail.com>
2024-11-10 23:22:17 -0400

    feat: mejora Input para tipo email

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,15 +1,16 @@
 import { FieldValues } from "react-hook-form";
 
 import { useState } from "react";
-import { PASSWORD, TEXT } from "@utils/const";
+import { EMAIL, PASSWORD, TEXT } from "@utils/const";
 
 import eye from "@assets/icons/eye.svg";
 import eyeOff from "@assets/icons/eye-off.svg";
+import mail from "@assets/icons/mail.svg";
 
 const InputBase = <T extends FieldValues> (props:InputProps<T>) => {
   const { identify, register, required=false, className="", type=TEXT, ...other } = props;
   return (
-    <input {...other}  id={identify} className={`${type===PASSWORD && "w-full"} p-1 m-1 rounded border-solid border border-zinc-200 ${className}`} {...register(identify, {required})}  /> 
+    <input {...other}  id={identify} className={`${type===PASSWORD && "pr-8 w-full"} ${type===EMAIL && "pl-8 w-full"} p-1 m-1 rounded border-solid border border-zinc-200 ${className}`} {...register(identify, {required})}  /> 
   );
 }
 
@@ -25,6 +26,19 @@ const Input = <T extends FieldValues> (props:InputProps<T>) => {
 					src={isView ? eyeOff : eye}
 					alt={isView ? "eyeOff" : "eye"}
 					className="absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer"
+					onClick={() => setIsView(!isView)}
+				/>
+			</div>
+		);
+	}
+	if(type === EMAIL) {
+		return (
+			<div className="relative flex w-full">
+				<InputBase  {...props} />
+				<img
+					src={mail}
+					alt={"email"}
+					className="absolute left-2 top-1/2 -translate-y-1/2 cursor-pointer"
 					onClick={() => setIsView(!isView)}
 				/>
 			</div>
